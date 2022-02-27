8736599a8999695d0548e772463f442618343f1b
Tom de Bruijn <tom@tomdebruijn.com>
2022-02-27 14:21:31 +0100

    Set up base Content-Security-Policy
    Set up a Content-Security-Policy that only allows resources from the
    same domain and Plausible for analytics. This can be expanded upon if I
    ever add resources from other domains. This should be kept to a minimum.

--- plugins/page_metadata.rb
+++ plugins/page_metadata.rb
@@ -1,3 +1,5 @@
+require "digest"
+
 module PageMetadata
   def canonical_url
     view.resource.destination.absolute_url.sub(/\/$/, "")
@@ -22,6 +24,24 @@ module PageMetadata
 
   def twitter_card_type
     "summary"
+  end
+
+  def csp_policy
+    if Bridgetown.env.development?
+      script_src = " 'unsafe-inline'"
+      style_src = " 'unsafe-inline'"
+    else
+      script_src = " 'sha256-#{plausible_script_sha256}'"
+    end
+    raw "default-src 'none'; script-src 'self' https://plausible.io #{script_src}; connect-src 'self' https://plausible.io; img-src 'self'; style-src 'self'#{style_src}; base-uri 'self'; form-action 'self'"
+  end
+
+  def plausible_script
+    "window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }"
+  end
+
+  def plausible_script_sha256
+    Digest::SHA256.base64digest(plausible_script)
   end
 end
 
