010f5455b277fc0f2b7b2da9cd8d2d40e4d3e90f
Tom de Bruijn <tom@tomdebruijn.com>
2022-03-05 19:57:44 +0100

    Mark features that are not yet released
    Features like the color output are not yet available, but I have written
    documentation for it. When that happens, show a different version label
    that informs the readers the feature is not yet available.

--- plugins/version_label.rb
+++ plugins/version_label.rb
@@ -1,10 +1,20 @@
 module VersionLabel
   def version_label(version)
-    raw(<<~HTML)
-      <div class="version-label" title="This feature was added in version #{version}.">
-        Version #{version}
-      </div>
-    HTML
+    latest_verison = Gem::Version.new(site.metadata.latest_version)
+    release_version = Gem::Version.new(version)
+    if latest_verison >= release_version
+      raw(<<~HTML)
+        <div class="version-label" title="This feature was added in version #{version}.">
+          Version #{version}
+        </div>
+      HTML
+    else
+      raw(<<~HTML)
+        <div class="version-label" title="This feature is not yet release">
+          📦 Not yet released
+        </div>
+      HTML
+    end
   end
 end
 
